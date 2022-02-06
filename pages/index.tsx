b6cc8aa0b4bc3ef4bd7d03e8c00aa72df9ac7538
fushaoqin-devops <97069562+fushaoqin-devops@users.noreply.github.com>
2022-02-06 16:47:57 -0500

    Dev: cypress testing (#3)
    * added cypress and added test coverage for home page
    
    * added ci checks

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,36 +32,28 @@ const Home: NextPage<Products> = (props: Products) => {
   if (!mounted) return null;
 
   return (
-    <div className="bg-gray-200 h-screen dark:bg-slate-900">
+    <div className="h-screen bg-gray-200 dark:bg-slate-900">
       <Head>
         <title>Home - FDA Drug Alternative</title>
         <meta name="description" content="FDA drug search engine" />
       </Head>
-      <div className="w-full flex justify-end">
+      <div className="flex w-full justify-end">
         <button
           aria-label="Toggle Dark Mode"
           type="button"
-          className="p-3 h-12 w-12 order-2 md:order-3 items-end"
+          className="order-2 h-12 w-12 items-end p-3 md:order-3"
           onClick={handleTheme}
         >
-          {theme === "dark" ? (
-            <Image
-              src="/moon.svg"
-              width="100%"
-              height="100%"
-              alt="dark theme icon"
-            ></Image>
-          ) : (
-            <Image
-              src="/sun.svg"
-              width="100%"
-              height="100%"
-              alt="light theme icon"
-            ></Image>
-          )}
+          <Image
+            src={theme === "dark" ? "/moon.svg" : "/sun.svg"}
+            width="100%"
+            height="100%"
+            alt="theme icon"
+            className="theme-icon"
+          ></Image>
         </button>
       </div>
-      <h1 className="text-center text-slate-900 font-bold py-24 text-6xl tracking-tight dark:text-gray-200">
+      <h1 className="py-24 text-center text-6xl font-bold tracking-tight text-slate-900 dark:text-gray-200">
         Affordable FDA Drug Alternative
       </h1>
       <SearchBar productList={props.productList} />
