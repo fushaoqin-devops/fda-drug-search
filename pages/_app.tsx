e084395caac8d2d15186ef53e167662d298f2f8d
fushaoqin-devops <97069562+fushaoqin-devops@users.noreply.github.com>
2022-04-04 17:12:07 -0400

    Feature - loading animation (#7)
    * added page transition
    
    * adjust size of loading icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { useState } from "react";
+import { RingLoader } from "react-spinners";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 
 declare global {
   var mongoose: any;
 }
 
+NProgress.configure({ showSpinner: false, easing: "ease" });
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const [pageLoading, setPageLoading] = useState(false);
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+    setPageLoading(true);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+    setPageLoading(false);
+  });
+  Router.events.on("routeChangeError", () => {
+    NProgress.done();
+    setPageLoading(false);
+  });
   return (
     <div>
       <Head>
@@ -17,7 +37,18 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <ThemeProvider attribute="class" enableSystem={false}>
-        <Component {...pageProps} />
+        {pageLoading ? (
+          <div className="flex h-screen items-center justify-center bg-gray-200 dark:bg-slate-900">
+            <div className="flex flex-col">
+              <RingLoader color={"#a5b4fc"} size={150} />
+              <span className="mx-auto my-8 animate-ping-slow font-mono tracking-widest text-slate-500 dark:text-gray-200">
+                Loading...
+              </span>
+            </div>
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </ThemeProvider>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "ping-slow": "ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite",
+      },
+    },
   },
   darkMode: "class",
   plugins: [require("tw-elements/dist/plugin")],
