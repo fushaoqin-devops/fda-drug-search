f5d31b28440be3373b75726ec22cdd464c2bd264
fushaoqin-devops <97069562+fushaoqin-devops@users.noreply.github.com>
2022-04-12 23:53:02 -0400

    add footer (#12)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useTheme } from "next-themes";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 
@@ -21,7 +22,7 @@ const Home: NextPage = () => {
   if (!mounted) return null;
 
   return (
-    <div className="h-screen bg-gray-200 dark:bg-slate-700">
+    <div className="relative h-screen bg-gray-200 dark:bg-slate-700">
       <Head>
         <title>Home - FDA Drug Alternative</title>
         <meta name="description" content="FDA drug search engine" />
@@ -48,6 +49,27 @@ const Home: NextPage = () => {
       <div className="mx-auto max-w-7xl">
         <SearchBar />
       </div>
+      <footer className="absolute bottom-0 w-full bg-gray-200 text-center text-gray-600 dark:bg-slate-700 lg:text-left">
+        <hr className="border-gray-300" />
+        <div className="flex items-center justify-center justify-between border-b border-gray-300 p-6">
+          <div></div>
+          <div className="self-center text-center">
+            <span>
+              © 2021 Copyright: <strong>ISTE 612 Team 5</strong>
+            </span>
+          </div>
+          <div className="flex justify-center">
+            <Link
+              href="https://github.com/fushaoqin-devops/fda-drug-search"
+              passHref
+            >
+              <a>
+                <Image src="/github.png" alt="" width={25} height={25} />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
